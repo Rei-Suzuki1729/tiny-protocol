686cdd70265baccb0dbba1533cef708331726559
Rei-Suzuki1729 <eva2021end@gmail.com>
2023-03-18 18:38:32 +0900

    create Protocol_management

--- net.c
+++ net.c
@@ -8,6 +8,9 @@
 #include "util.h"
 #include "net.h"
 
+#include "ip.h"
+
+
 struct net_protocol {
     struct net_protocol *next;
     uint16_t type;
@@ -112,12 +115,50 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
 int
 net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t len, struct net_device *dev))
 {
+    struct net_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            errorf("already registered, type=0x%04x", type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if (!proto) {
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("registered, type=0x%04x", type);
+    return 0;
+
 }
 
 int
 net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (proto->type == type) {
+
+
+
+
+
+            debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zu",
+                proto->queue.num, dev->name, type, len);
+            debugdump(data, len);
+            return 0;
+        }
+    }
+    /* unsupported protocol */
     return 0;
+
 }
 
 int
@@ -157,6 +198,11 @@ net_init(void)
         errorf("intr_init() failure");
         return -1;
     }
+    if (ip_init() == -1) {
+        errorf("ip_init() failure");
+        return -1;
+    }
+
     infof("initialized");
     return 0;
 }
